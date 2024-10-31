c4890898d3bf49ec02e8984e527649085ead85ea
abeabebawshita <abebawshita19@gmail.com>
2024-10-31 01:46:03 -0700

    Update Splash.java

--- electricity/Splash.java
+++ electricity/Splash.java
@@ -1,51 +1,117 @@
 
 package electricity;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
 
-public class Splash extends JFrame implements Runnable {
-    Thread t;
-    Splash() {
+public class Login extends JFrame implements ActionListener{
+
+    JButton login, cancel, signup;
+  
+    
+    JTextField username, password;
+    Choice logginin;
+    Login() {
+        super("Login Page");
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
         
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/background.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(730, 550, Image.SCALE_DEFAULT);
-        ImageIcon i3 = new ImageIcon(i2);
-        JLabel image = new JLabel(i3);
+        JLabel lblusername = new JLabel("Username");
+        lblusername.setBounds(300, 20, 100, 20);
+        add(lblusername);
+        
+        username = new JTextField();
+        username.setBounds(400, 20, 150, 20);
+        add(username);
+        
+        JLabel lblpassword = new JLabel("Password");
+        lblpassword.setBounds(300, 60, 100, 20);
+        add(lblpassword);
+        
+        password = new JTextField();
+        password.setBounds(400, 60, 150, 20);
+        add(password);
+        
+        JLabel loggininas = new JLabel("Loggin in as");
+        loggininas.setBounds(300, 100, 100, 20);
+        add(loggininas);
+        
+        logginin = new Choice();
+        logginin.add("Admin");
+        logginin.add("Customer");
+        logginin.setBounds(400, 100, 150, 20);
+        add(logginin);
+        
+        login = new JButton("Login");
+        login.setBounds(330, 160, 150, 40);
+        login.setBackground(Color.green);
+        login.addActionListener(this);
+        add(login);
+        
+        cancel = new JButton("Cancel");
+        cancel.setBounds(550, 160, 200, 40);
+        cancel.setBackground(Color.red);
+        cancel.addActionListener(this);
+        add(cancel);
+         JLabel account = new JLabel("if there is no account");
+        account.setBounds(300, 220, 170, 40);
+        add(account);
+        
+        signup = new JButton("Signup");
+        signup.setBounds(450, 220, 150, 40);
+        signup.setBackground(Color.CYAN);
+    
+        signup.addActionListener(this);
+        add(signup);
+        
+        ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("icon/second.jpg"));
+        Image i8 = i7.getImage().getScaledInstance(250, 250, Image.SCALE_DEFAULT);
+        ImageIcon i9 = new ImageIcon(i8);
+        JLabel image = new JLabel(i9);
+        image.setBounds(0, 0, 250, 250);
         add(image);
         
-        setVisible(true);
-        
-        int x = 1;
-        for (int i = 2; i < 600; i+=4, x+=1) {
-            setSize(i + x, i);
-            setLocation(700 - ((i + x)/2), 400 - (i/2));
-            try {
-                Thread.sleep(5);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        
-        t = new Thread(this);
-        t.start();
-        
+        setSize(900, 600);
+        setLocation(400, 200);
         setVisible(true);
     }
     
-    public void run() {
-        try {
-            Thread.sleep(7000);
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == login) {
+            String susername = username.getText();
+            String spassword = password.getText();
+            String user = logginin.getSelectedItem();
+            
+            try {
+                Conn c = new Conn();
+                String query = "select * from login where username = '"+susername+"' and password = '"+spassword+"' and user = '"+user+"'";
+                
+                ResultSet rs = c.s.executeQuery(query);
+                
+                if (rs.next()) {
+                    String meter = rs.getString("meter_no");
+                    setVisible(false);
+                    new Project(user, meter);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invalid Login");
+                    username.setText("");
+                    password.setText("");
+                }
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (ae.getSource() == cancel) {
+            setVisible(false);
+        } else if (ae.getSource() == signup) {
             setVisible(false);
             
-            // login frame
-           new Login();
-        } catch (Exception e) {
-            e.printStackTrace();
+            new Signup();
         }
     }
     
     public static void main(String[] args) {
-        new Splash();
+        new Login();
     }
 }
