870ea6c9cb4e431733b5be6a0bf2e39278b76c2e
abeabebawshita <abebawshita19@gmail.com>
2024-10-31 01:49:11 -0700

    Update Splash.java

--- electricity/Splash.java
+++ electricity/Splash.java
@@ -1,117 +1,51 @@
 
 package electricity;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.sql.*;
 
-public class Login extends JFrame implements ActionListener{
-
-    JButton login, cancel, signup;
-  
-    
-    JTextField username, password;
-    Choice logginin;
-    Login() {
-        super("Login Page");
-        getContentPane().setBackground(Color.WHITE);
-        setLayout(null);
-        
-        JLabel lblusername = new JLabel("Username");
-        lblusername.setBounds(300, 20, 100, 20);
-        add(lblusername);
-        
-        username = new JTextField();
-        username.setBounds(400, 20, 150, 20);
-        add(username);
-        
-        JLabel lblpassword = new JLabel("Password");
-        lblpassword.setBounds(300, 60, 100, 20);
-        add(lblpassword);
-        
-        password = new JTextField();
-        password.setBounds(400, 60, 150, 20);
-        add(password);
-        
-        JLabel loggininas = new JLabel("Loggin in as");
-        loggininas.setBounds(300, 100, 100, 20);
-        add(loggininas);
-        
-        logginin = new Choice();
-        logginin.add("Admin");
-        logginin.add("Customer");
-        logginin.setBounds(400, 100, 150, 20);
-        add(logginin);
-        
-        login = new JButton("Login");
-        login.setBounds(330, 160, 150, 40);
-        login.setBackground(Color.green);
-        login.addActionListener(this);
-        add(login);
-        
-        cancel = new JButton("Cancel");
-        cancel.setBounds(550, 160, 200, 40);
-        cancel.setBackground(Color.red);
-        cancel.addActionListener(this);
-        add(cancel);
-         JLabel account = new JLabel("if there is no account");
-        account.setBounds(300, 220, 170, 40);
-        add(account);
-        
-        signup = new JButton("Signup");
-        signup.setBounds(450, 220, 150, 40);
-        signup.setBackground(Color.CYAN);
-    
-        signup.addActionListener(this);
-        add(signup);
-        
-        ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("icon/second.jpg"));
-        Image i8 = i7.getImage().getScaledInstance(250, 250, Image.SCALE_DEFAULT);
-        ImageIcon i9 = new ImageIcon(i8);
-        JLabel image = new JLabel(i9);
-        image.setBounds(0, 0, 250, 250);
+public class Splash extends JFrame implements Runnable {
+    Thread t;
+    Splash() {
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/background.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(800, 700, Image.SCALE_SMOOTH);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
         add(image);
         
-        setSize(900, 600);
-        setLocation(400, 200);
         setVisible(true);
-    }
-    
-    public void actionPerformed(ActionEvent ae) {
-        if (ae.getSource() == login) {
-            String susername = username.getText();
-            String spassword = password.getText();
-            String user = logginin.getSelectedItem();
-            
+        
+        int x = 1;
+        for (int i = 2; i < 600; i+=4, x+=1) {
+            setSize(i + x, i);
+            setLocation(700 - ((i + x)/2), 400 - (i/2));
             try {
-                Conn c = new Conn();
-                String query = "select * from login where username = '"+susername+"' and password = '"+spassword+"' and user = '"+user+"'";
-                
-                ResultSet rs = c.s.executeQuery(query);
-                
-                if (rs.next()) {
-                    String meter = rs.getString("meter_no");
-                    setVisible(false);
-                    new Project(user, meter);
-                } else {
-                    JOptionPane.showMessageDialog(null, "Invalid Login");
-                    username.setText("");
-                    password.setText("");
-                }
-                
+                Thread.sleep(5);
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        } else if (ae.getSource() == cancel) {
-            setVisible(false);
-        } else if (ae.getSource() == signup) {
+        }
+        
+        t = new Thread(this);
+        t.start();
+        
+        setVisible(true);
+    }
+    
+    public void run() {
+        try {
+            Thread.sleep(700);
             setVisible(false);
             
-            new Signup();
+            // login frame
+           new Login();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
     
     public static void main(String[] args) {
-        new Login();
+        System.out.print("WELLCOM TO ETHIOPIAN ELECTICITY DESCRITE");
+        new Splash();
     }
 }
